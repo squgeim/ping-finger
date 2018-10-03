cfd9b7b2ebd671ec633230ec3b6d518628ca8fe0
Deepak Adhikari <deeps.adhi@gmail.com>
2018-10-03 00:42:02 -0500

    Add finger and echo client

---
+++ finger-client.py
@@ -0,0 +1,95 @@
+import sys
+import socket 
+ 
+FINGER_PROTOCOL_PORT= 79
+
+
+def finger(ip_address, args=""):
+    '''
+    Ping an ip address.
+
+    Args:
+        ip_address (string): IP Address
+    
+    Returns:
+        bool: True for node details, False otherwise.
+    '''
+
+    BUFFER_SIZE = 1024
+     
+    try:
+        # Create a socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        
+        # Connect to the IP address and port
+        sock.connect((ip_address, FINGER_PROTOCOL_PORT))
+
+        # Send finger arguments
+        sock.send(args + "\n")
+
+        while 1:
+            buffer = sock.recv(1024)
+
+            if not buffer:
+                break
+
+            sys.stdout.write(buffer)
+
+        sys.stdout.flush()
+            
+        return True
+    except:
+        return False
+
+def get_ip_address(host):
+    '''
+    Resolve IP address of a host 
+
+    Args:
+        host (string): Host name
+    Returns
+        string bool: IP address on success, False otherwise.
+    '''
+
+    try:
+        addr_info = socket.getaddrinfo(host, FINGER_PROTOCOL_PORT)
+
+        if len(addr_info) == 0:
+            return False
+        
+        return addr_info[0][4][0]
+    except:
+        return False
+
+if __name__ == "__main__":
+    if len(sys.argv) not in [1, 2]:
+        print "Usage: finger-client [user][user@host]"
+        sys.exit()
+
+    if len(sys.argv) == 1:
+        host = "127.0.0.1"
+        args = ""
+    elif len(sys.argv) == 2:
+        connection = sys.argv[1].split('@')
+        
+        if len(connection) == 1:
+            host = "127.0.0.1"
+            args = sys.argv[1]
+        elif len(connection) == 2:
+            host = connection[1]
+            args = connection[0]
+        else:
+            print "Error: invalid parameter" 
+            sys.exit()
+    
+    ip_address = get_ip_address(host)
+
+    response = finger(ip_address, args)
+
+    if (response == False):
+        print "Error! could not fetch details"
+    else:
+        print response
+
+
+
---
+++ ping-client
@@ -0,0 +1,73 @@
+#!/usr/bin/python2
+import sys
+import socket 
+ 
+ECHO_SERVER_PORT = 7
+
+
+def ping(ip_address):
+    '''
+    Ping an ip address.
+
+    Args:
+        ip_address (string): IP Address
+    
+    Returns:
+        bool: True for success, False otherwise.
+    '''
+
+    MESSAGE = "ping" 
+    BUFFER_SIZE = 1024
+     
+    try:
+        # Create a socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        
+        # Connect to the IP address and port
+        sock.connect((ip_address, ECHO_SERVER_PORT))
+        
+        # Send echo message
+        sock.sendall(MESSAGE) 
+        
+        # Wait for the response
+        response = sock.recv(BUFFER_SIZE) 
+
+        return True
+    except:
+        return False
+
+def get_ip_address(host):
+    '''
+    Resolve IP address of a host 
+
+    Args:
+        host (string): Host name
+    Returns
+        string bool: IP address on success, False otherwise.
+    '''
+
+    try:
+        addr_info = socket.getaddrinfo(host, ECHO_SERVER_PORT)
+
+        if len(addr_info) == 0:
+            return False
+        
+        return addr_info[0][4][0]
+    except:
+        return False
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "Usage: ping-client destination"
+        sys.exit()
+
+    host = sys.argv[1]
+    ip_address = get_ip_address(host)
+
+    if ping(ip_address) == True:
+        print host + " is alive"
+    else:
+        print host + " is unreachable"
+
+
+
